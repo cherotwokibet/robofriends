1e9b889e84301dc141463c9c0437707aadad81f4
cherotwokibet <bkcherotwo@gmail.com>
2022-11-20 18:53:33 +0300

    Add Search Box

--- src/App.js
+++ src/App.js
@@ -3,11 +3,33 @@ import Card from './Card';
 import 'tachyons'
 import {robots} from './robots'
 import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { useState } from 'react';
 
 function App() {
+  // const state = {
+  //   robots: robots,
+  //   searchField : ''
+  // }
+  const [robos,setRobots] = useState(robots);
+  const [searchfield,setSearchField] = useState('');
+
+  const onSearchChange = (e) => {
+    // console.log(e.target.value)
+    
+    setSearchField(e.target.value)
+    // console.log(filteredRobots)
+  }
+  const filteredRobots = robots.filter(robots => {
+    return robots.name.toLowerCase().includes(searchfield.toLowerCase())
+  })
+
+
   return (
-    <div>
-      <CardList robots={robots}/>
+    <div className='tc'>
+      <h1>RoboFriends</h1>
+      <SearchBox searchChange={onSearchChange}/>
+      <CardList robots={filteredRobots}/>
     </div>
   );
 }
